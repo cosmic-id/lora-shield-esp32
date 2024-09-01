5ff90b3fe0711ca41ee12a78d35f3a2b210883ab
cosmic-id <cosmic.indonesia01@gmail.com>
2024-09-01 16:26:06 +0700

    add lora p2p transmitter

---
+++ lora-p2p-transmitter/lora-p2p-transmitter.ino
@@ -0,0 +1,55 @@
+/*********
+  Modified from the examples of the Arduino LoRa library
+  More resources: https://github.com/cosmic-id/
+*********/
+
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the transceiver module
+#define NUON_V1_NSS 16
+#define NUON_V2_RST 13
+#define NUON_V2_DIO0 17
+
+int counter = 0;
+
+void setup() {
+
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Sender");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(LORA_AURORA_V2_NSS, LORA_AURORA_V2_RST, LORA_AURORA_V2_DIO0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //866E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(920E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+  
+  // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(2000);
+}
